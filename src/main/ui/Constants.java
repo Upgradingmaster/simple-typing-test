2ec2150014a8e61affeddc9d1525fb5019b099a0
Armaan Sameer <armaansameer2005@gmail.com>
2024-11-21 15:11:03 -0800

    Added static Constants class for global access

---
+++ src/main/ui/Constants.java
@@ -0,0 +1,14 @@
+package ui;
+
+class Constants {
+    public static final String defaultError = "\n\t---Invalid Selection---\n";
+    public static final int mainMenuOptionCount = 6;
+
+    public static final int countdownTime = 5;
+    public static final String wordsFilePath = "data/words.txt";
+    public static final int wordsMax = 25;
+    public static final int timeMax = 30;
+
+    public static final String stateFilePath = "data/state.json";
+    //public static final String stateFilePath = "data/testMultipleStatsState.json";
+}
