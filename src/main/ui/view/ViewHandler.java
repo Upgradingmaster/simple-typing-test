2a0423e233a461b5c6c57dafc1308a966400b494
Armaan Sameer <armaansameer2005@gmail.com>
2024-11-21 11:38:42 -0800

    Added specification for ViewHandler class

--- src/main/ui/view/ViewHandler.java
+++ src/main/ui/view/ViewHandler.java
@@ -1,34 +1,75 @@
 package ui;
 
-public class ViewHandler {
-    enum GraphState {
-        ON, OFF
+import java.awt.Dimension;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+
+import model.Statistic;
+import model.Statistics;
+
+// Top level Handler for managing state and user interaction with the various view
+class ViewHandler implements PropertyChangeListener {
+    private boolean graphState = false;
+    private JFrame frame;
+
+    private MainView mainView;
+    private StatisticsView statisticView; 
+    private JSplitPane splitPane; 
+
+    ViewHandler(JFrame frame) {
+        this.frame = frame;
     }
 
-    public GraphState graphState;
+    // EFFECTS: Initializes the primary child views
+    public void initViews() {
+        
+    }
+
+    // EFFECTS: Reloads all the views with latest ui changes
+    public void reloadViews(){
+       
+    }
 
 
     // EFFECTS: Swaps the Main Panel with the GraphView or the MainMenuView
     public void toggleGraph() {
-        if (graphState == GraphState.ON) {
-            renderGraph();
-        } else renderMainMenu();
-        graphState = !graphState;
+        
     }
 
 
     // REQUIRES: graphState == GraphState.OFF
     // EFFECTS: Sets the main panel to display the main menu
     private void renderMainMenu() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'renderMainMenu'");
+
     }
 
 
     // REQUIRES: graphState == GraphState.ON
     // EFFECTS: Sets the main panel to display the graph
     private void renderGraph() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'renderGraph'");
+
+    }
+
+
+    // EFFECTS: Handles ui updates for child views whenever state changes 
+    @Override
+    public void propertyChange(PropertyChangeEvent pce) {
+        
+    }
+
+    // EFFECTS: Adds the Statistic Button at the end of the view
+    private void updateFrameAddStat(Statistic statistic) {
+    }
+
+    // EFFECTS: Removes the Statistic Button at the index
+    private void updateFrameRemoveStat(int index) {
+    }
+
+    // EFFECTS: Sets the Buttons to match new Statistics object
+    private void updateFrameSetStats(Statistics statistics) {
     }
 }
