d0f92a3c6e892fafbe5c829751312089335a0d1d
Armaan Sameer <armaansameer2005@gmail.com>
2024-11-21 10:19:09 -0800

    Added Specification for State class which is being factored out into its own class

---
+++ src/main/ui/State.java
@@ -0,0 +1,30 @@
+package ui;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+class State {
+    private Statistics stats;
+
+    private PropertyChangeSupport support;
+
+
+    State(){
+    }
+
+
+
+    public void addPropertyChangeListener(PropertyChangeListener pcl) {
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener pcl) {
+    }
+
+    public void addStatistic(Statistic stat) {
+    }
+
+
+    public void addStatistic(Statistic stat) {
+    }
+
+}
