c6462d1bad24e6e84e481db8de1494fd1c948eeb
Armaan Sameer <armaansameer2005@gmail.com>
2024-11-21 11:29:57 -0800

    Added helper methods in StatisticsView class to update the ui

--- src/main/ui/view/StatisticsView.java
+++ src/main/ui/view/StatisticsView.java
@@ -11,6 +11,7 @@ import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
 import model.Statistic;
+import model.Statistics;
 
 // The pane displaying all current statistics i.e. the state
 public class StatisticsView extends JPanel {
@@ -23,7 +24,6 @@ public class StatisticsView extends JPanel {
        this.controller = controller;
        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
        noStatistics();
-       //initComponents();
     }
 
     //private void initComponents() {
@@ -31,15 +31,38 @@ public class StatisticsView extends JPanel {
     //    add(new StatisticButton(new Statistic("abc", "abc", 2, 2)));
     //    add(new StatisticButton(new Statistic("abc", "abc", 2, 2)));
     //}
+    
+    // EFFECTS: Adds a statistic and updates the ui
+    //          Deletes the No statisics message if it exists
+    public void addStatisticButton(Statistic statistic) {
+        if (getComponentCount() == 1 && getComponent(0) instanceof JLabel) {
+            remove(0);
+        }
+        add(new StatisticButton(statistic));
+    }
+
+    public void removeStatisticButton(int index) {
+        remove(index);
+        if (getComponentCount() == 0) {
+            noStatistics();
+        }
+    }
+
+    public void clearStatisticButtons() {
+        removeAll();
+        noStatistics();
+    }
+
+
+    public void setStatisticsButton(Statistics statistics) {
+        clearStatisticButtons();
+        for (Statistic s : statistics.getStatsArrayList()){
+            addStatisticButton(s);
+        }
+    }
 
     // EFFECTS: Display no statistics loaded when state is empty
     private void noStatistics() {
         add(new JLabel("No statistics loaded"));
     }
-
-    // EFFECTS: Adds a statistic and updates the ui
-    //          Deletes the No statisics message if it exists
-    public void addStatistic(Statistic statistic){
-    }
-
 }
