a8c51a0110e1be8db88183fabea73c88e41a3eba
Armaan Sameer <armaansameer2005@gmail.com>
2024-11-21 12:05:35 -0800

    Added Specification for MainView class

--- src/main/ui/view/MainView.java
+++ src/main/ui/view/MainView.java
@@ -1,5 +1,6 @@
 package ui;
 
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 
@@ -9,13 +10,31 @@ import javax.swing.border.LineBorder;
 
 // Represents the main view which will either be the main menu or the graph
 public class MainView extends JPanel {
+    private HomeView homeView;
+    private GraphView graphView;
+    private CardLayout cardLayout;
 
-    //MainView() {
-    //    add(new JLabel("abskda"));
-    //    add(new JLabel("abskda"));
-    //    add(new JLabel("abskda"));
-    //    add(new JLabel("abskda"));
-    //    add(new JLabel("abskda"));
-    //}
+    MainView() {
+    }
 
+    //EFFECTS: Defines the to child views, Home and Graph
+    private void initViews() {
+    }
+
+    // EFFECTS: Registers the two views into the card layout
+    //          showing Home as default
+    private void initCardLayout() {
+    }
+
+    // EFFECTS: Sets the view to home 
+    public void showHomeView() {
+    }
+
+    // EFFECTS: Sets the view to graph
+    public void showGraphView() {
+    }
+
+    // EFFECTS: Toggles the view from home to graph and vice versa
+    public void toggleView() {
+    }
 }
